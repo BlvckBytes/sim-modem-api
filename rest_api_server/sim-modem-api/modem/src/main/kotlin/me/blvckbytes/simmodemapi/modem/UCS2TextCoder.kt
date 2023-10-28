4be592efe3318ea42f4a28f903132305518058c9
Philip Hell <blvckbytes@gmail.com>
2023-10-29 00:10:00 +0200

    added decode methods to the available text coders

--- rest_api_server/sim-modem-api/modem/src/main/kotlin/me/blvckbytes/simmodemapi/modem/UCS2TextCoder.kt
+++ rest_api_server/sim-modem-api/modem/src/main/kotlin/me/blvckbytes/simmodemapi/modem/UCS2TextCoder.kt
@@ -1,8 +1,10 @@
 package me.blvckbytes.simmodemapi.modem
 
 import me.blvckbytes.simmodemapi.domain.exception.IllegalCharacterException
+import java.nio.ByteBuffer
 import java.nio.CharBuffer
 import java.nio.charset.CodingErrorAction
+import java.nio.charset.MalformedInputException
 import java.nio.charset.UnmappableCharacterException
 
 object UCS2TextCoder {
@@ -12,14 +14,31 @@ object UCS2TextCoder {
     .onMalformedInput(CodingErrorAction.REPORT)
     .onUnmappableCharacter(CodingErrorAction.REPORT)
 
+  private val decoder = Charsets.UTF_16BE
+    .newDecoder()
+    .onMalformedInput(CodingErrorAction.REPORT)
+    .onUnmappableCharacter(CodingErrorAction.REPORT)
+
   fun encode(value: String): ByteArray {
     try {
       val result = encoder.encode(CharBuffer.wrap(value))
       val byteArray = ByteArray(result.capacity())
       result.get(byteArray)
       return byteArray
-    } catch (exception: UnmappableCharacterException) {
-      throw IllegalCharacterException()
+    } catch (exception: Exception) {
+      when (exception) {
+        is UnmappableCharacterException, is MalformedInputException -> throw IllegalCharacterException()
+        else -> throw exception
+      }
+    }
+  }
+
+  fun decode(value: ByteArray): String? {
+    return try {
+      val result = decoder.decode(ByteBuffer.wrap(value))
+      result.toString()
+    } catch (exception: Exception) {
+      null
     }
   }
 }
