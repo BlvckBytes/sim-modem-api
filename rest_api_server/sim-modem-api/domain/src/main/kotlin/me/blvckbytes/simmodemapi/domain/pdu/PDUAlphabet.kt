713c850c7dafedef66d913edb5822a6341ccb278
Philip Hell <blvckbytes@gmail.com>
2023-12-28 12:53:41 +0100

    corrected MAX_UD_LENGTH constant

--- rest_api_server/sim-modem-api/domain/src/main/kotlin/me/blvckbytes/simmodemapi/domain/pdu/PDUAlphabet.kt
+++ rest_api_server/sim-modem-api/domain/src/main/kotlin/me/blvckbytes/simmodemapi/domain/pdu/PDUAlphabet.kt
@@ -20,7 +20,7 @@ enum class PDUAlphabet(
     // The maximum length of the payload (called UD - User Data) dictates how many
     // characters may be carried within one short message, depending on it's encoding
     // as well as of the presence or absence of a header
-    private const val MAX_UD_LENGTH = 1200
+    private const val MAX_UD_LENGTH = 1120
 
     val AVAILABLE_ALPHABETS_ASCENDING = arrayOf(
       GSM_SEVEN_BIT,
